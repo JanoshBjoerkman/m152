e9025b8f2e3b315abc7f830e5391e74ac81e01d0
Janosh.Bjoerkman <janosh.b@bluewin.ch>
2017-09-06 12:30:54 +0200

    some excercises

---
+++ u7_BootstrapNavbar/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewpot" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/my.css">
+    <title>Playground Modul 152</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">Hella lit navbar</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">VIP</a></li>
+          <li><a href="#">Peasant</a></li>
+          <li><a href="#">Mofo</a></li>
+          <li>
+            <a href="#" class="btn btn-link">
+              <span class="glyphicon glyphicon-pencil"></span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <nav class="navbar navbar-inverse navbar-fixed-bottom">
+      <div class="container-fluid">
+        <a href="#" class="btn btn-link">
+          <span class="fa fa-3x fa-instagram" aria-hidden="true"></span>
+        </a>
+        <a href="#" class="btn btn-link">
+          <span class="fa fa-3x fa-twitter" aria-hidden="true"></span>
+        </a>
+        <a href="#" class="btn btn-link">
+          <span class="fa fa-3x fa-reddit" aria-hidden="true"></span>
+        </a>
+      </div>
+    </nav>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </body>
+</html>
