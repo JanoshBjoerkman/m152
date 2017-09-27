063a5bf56528f7b3b83e1d5da0865851289c8c3f
Janosh.Bjoerkman <janosh.b@bluewin.ch>
2017-09-27 12:29:58 +0200

    alphachannel exercise

---
+++ exercises/u12_Bootstrap_Alpha_Channel/own.js
@@ -0,0 +1,5 @@
+function changeBgColor() {
+    var element = document.getElementById("eagle-box");
+    var colorInput = document.getElementById("bg-selector");
+    element.style.backgroundColor = colorInput.value;
+}
